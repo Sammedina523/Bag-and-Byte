3be9c73cd53560b36e3bfcb39d05bc323c41bd71
Samantha A. Medina <smedina1@pride.hofstra.edu>
2024-10-21 19:29:11 -0400

    Test

--- src/app1.py
+++ src/app1.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-# PostgreSQL database configuration
+# PostgreSQL database configuration.
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://grocery_user:csc190@localhost/grocery_db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
