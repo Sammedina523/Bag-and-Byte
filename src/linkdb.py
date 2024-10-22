2d79ae3a40fbe0302da4f3b7b9fc7332293b631f
Imam M. Jah <ijah3@pride.hofstra.edu>
2024-10-21 20:27:33 -0400

    test

--- src/linkdb.py
+++ src/linkdb.py
@@ -4,7 +4,7 @@ from flask import Flask, jsonify
 from flask_sqlalchemy import SQLAlchemy
 
 # Set up Flask app and SQLAlchemy
-app = Flask(__name_)
+app = Flask(__name__)
 
 # PostgreSQL configuration
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://grocery_user:csc190@localhost/grocery_db'
